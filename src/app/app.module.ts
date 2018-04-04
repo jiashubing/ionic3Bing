67ab1eb9f1d914b91a5f6b190e7a817855966954
贾树丙 <1310059060@qq.com>
2018-04-04 09:28:14 +0800

    使用ionic做一个壳，内部嵌套一个网站

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,18 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
+import {MyApp} from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import {HomePage} from '../pages/home/home';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {ThemeableBrowser} from '@ionic-native/themeable-browser';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -26,15 +21,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
+    ThemeableBrowser,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
-export class AppModule {}
+export class AppModule {
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {ThemeableBrowser, ThemeableBrowserOptions} from "@ionic-native/themeable-browser";
+import {Events, NavController} from "ionic-angular";
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(private events: Events,
+              private nav: NavController,
+              private themeableBrowser: ThemeableBrowser) {
   }
+
+  ionViewDidEnter() {
+    setTimeout(() => {
+      this.run();
+    }, 1000);
+  }
+
+  f() {
+    this.run();
+  }
+
+  run() {
+    let options: ThemeableBrowserOptions = {
+      statusbar: {
+        color: '#ffffff'
+      },
+      toolbar: {
+        height: 0,
+        color: '#4876FF'
+      },
+      title: {
+        color: '#ffffff',
+        showPageTitle: false
+      }
+    };
+    this.themeableBrowser.create("https://jiashubing.cn", '_blank', options);
+  }
+
 
 }
